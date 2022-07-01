cc65f3c7e3c01487c38b1b6018d9a66e323dbc46
Mathéo <soyzer0@protonmail.com>
2022-07-01 17:10:24 +0200

    Testeo de funciones

--- src/funciones/walltext.js
+++ src/funciones/walltext.js
@@ -1,12 +1,17 @@
 function walltext(string) {
 
-    let regex = /\n/g;
-    if(string.match(regex || '').length > 8) return true;
+    var prueba = (string.match(/\n/g) || '').length;
+    if(prueba > 8) return true;
     else return false;
 
 }
 
-module.exports = walltext;
+function wltx2(string) {
+    string = string.replace(/\s+/g,"_");
+    return /(\S)(\1{5,})/g.test(string);
+}
+
+module.exports = walltext, wltx2;
 
 /**
  * @description El módulo detecta si una cadena de texto tiene más de 8 lineas.
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const links = require("./funciones/links.js");
 const whitelist = require("./funciones/whitelist.js");
-
+const walltext = require("./funciones/walltext.js");
+const wltx2 = require("./funciones/walltext.js");
 
 module.exports = {
     links,
-    whitelist
+    whitelist,
+    walltext,
+    wltx2
 }
