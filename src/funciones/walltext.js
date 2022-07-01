26bc9b0bb0247b249c1bacf931c796b0f56b23d9
Mathéo <soyzer0@protonmail.com>
2022-07-01 17:28:56 +0200

    a

--- src/funciones/walltext.js
+++ src/funciones/walltext.js
@@ -4,14 +4,13 @@ function walltext(string) {
     if(prueba > 8) return true;
     else return false;
 
-}
-
-function wltx2(string) {
     string = string.replace(/\s+/g,"_");
     return /(\S)(\1{5,})/g.test(string);
+
 }
 
-module.exports = walltext, wltx2;
+
+module.exports = walltext;
 
 /**
  * @description El módulo detecta si una cadena de texto tiene más de 8 lineas.
